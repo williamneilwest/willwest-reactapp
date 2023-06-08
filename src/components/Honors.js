8aa8450d8d9a36b640a06c39ba7ef456f017b27a
William West <williamneilwest@gmail.com>
2023-06-08 14:37:30 -0600

    switched order of projects and accomplishments

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -5,11 +5,11 @@ import hr from "../assets/curve-hr.svg"
 export default function Honors(){
     return (
         <div id="honors" className="mt-4 text-white">
-            <h1 className="text-2xl font-bold">Honors & Awards</h1>
-            <p className="font-light text-gray-400">Here are some of my honors and awards</p>
+            <h1 className="text-2xl font-bold">Accomplishments</h1>
+            <p className="font-light text-gray-400">Here are some of my Accomplishments</p>
 
             <div className="flex flex-col md:flex-row mt-4 gap-5">
-                <HonorCard name="Top 10 SELEKNAS ASEAN Skills Competition ITNSA 2022" issued="KEMENAKER RI" desc="Linux Server, Windows Server, Linux Routing, Cisco Troubleshooting, Cisco Network Analysis, Network and System Programmabillity, Containerized Service." />
+                <HonorCard name="The National Society of Leadership & Success" issued="NSLS, INC" desc="Through the National Society of Leadership and Success, I have been able to accomplished significant personal and professional growth. Through engaging workshops, inspiring guest speakers, and networking opportunities, I have honed my leadership skills, expanded my knowledge base, and built a strong network of like-minded individuals. This transformative institution has empowered me to step into leadership roles, develop my potential, and make a meaningful impact in my community." />
             </div>
             <img src={hr} className="w-full mt-8 md:h-2" alt="hr" />
         </div>

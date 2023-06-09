fcfc1b07af939c0a6c205172ee81ce7a53a77ae1
William West <williamneilwest@gmail.com>
2023-06-09 15:08:58 -0600

    added photo

--- src/components/Hiro.js
+++ src/components/Hiro.js
@@ -22,8 +22,8 @@ export default function Hiro () {
             null
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
-            <div className='md:w-3/6 md:p-4'>
-                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} style={{width: 450}} />
+            <div className='md:w-3/6 md:p-4 justify-center'>
+                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200" src={profile} alt="profile" onLoad={() => setLoaded(false)} style={{width: 250}} />
             </div>
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
                 <div className="flex flex-col w-full mt-8">

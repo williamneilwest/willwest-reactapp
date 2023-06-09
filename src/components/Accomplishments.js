ad659009cea9fe53bf88ca5aff4123a124fb5ee8
William West <williamneilwest@gmail.com>
2023-06-09 17:47:04 -0600

    added photo

--- src/components/Accomplishments.js
+++ src/components/Accomplishments.js
@@ -4,8 +4,8 @@ import hr from "../assets/curve-hr.svg"
 
 export default function Accomplishments(){
     return (
-        <div id="honors" className="mt-4 text-white">
-            <h1 className="text-2xl font-bold">Accomplishments</h1>
+        <div id="honors" className="mt-4 text-white flex flex-col items-center">
+            <h1 className="text-2xl font-bold items-center">Accomplishments</h1>
             <p className="font-light text-gray-400">A few things I'm proud to present!</p>
             <div className="flex flex-col md:flex-row mt-4 gap-5 justify-center">
                 <AccCard name="The National Society of Leadership & Success" issued="NSLS, INC" desc="Through the National Society of Leadership and Success, I have been able to accomplished significant personal and professional growth. Through engaging workshops, inspiring guest speakers, and networking opportunities, I have honed my leadership skills, expanded my knowledge base, and built a strong network of like-minded individuals. This transformative institution has empowered me to step into leadership roles, develop my potential, and make a meaningful impact in my community." />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,12 +9,13 @@ import personalpage from "../assets/personalpage.png"
 
 export default function Projects(){
     return (
-        <div id="certs" className="mt-4 text-white">
-            <h1 className="text-2xl font-bold">Projects</h1>
+        <div id="certs" className="mt-4 text-white flex flex-col items-center">
+            <h1 className="text-2xl font-bold ">Projects</h1>
             <p className="font-light text-gray-400">My Personal Projects, including Dashboard Visualizations, Back-end Programs, Front-end UIs and other proofs of concept.</p>
 
-            {/* <div className="flex flex-col md:flex-row flex-wrap mt-4 gap-5"> */}
-            <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
+            {/* <div className="flex flex-col md:flex-row flex-wrap mt-4 gap-5">
+            Change the number of columns below to place cards as desired*/}
+            <div className="grid grid-cols-1 md:grid-cols-2 mt-4 gap-5">
                 <ProjectCard name="Travlr Website" img={travlr} issued="Fullstack development using the MEAN stack." date="2023" url="https://github.com/williamneilwest/cs465-fullstack"/>
                 <ProjectCard name="Personal Website" img={personalpage} issued="The very page you are reading this on was created using React to provide a responsive, single-page application that is programmatically for scalability and brevity." date="2023" url="https://github.com/williamneilwest/willwest-reactapp"/>
             </div>

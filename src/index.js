674fd0748b20021c2e97f19daed1ae61ede4f171
William West <williamneilwest@gmail.com>
2023-06-09 18:33:22 -0600

    fixed photo

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'),
+    () => {
+        document.body.style.overscrollBehavior = 'contain';
+    }
+);
 root.render(
   <React.StrictMode>
     <App />

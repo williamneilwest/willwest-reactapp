34b073e739374c72a11d49b29f543be9117ee8d6
William West <williamneilwest@gmail.com>
2023-06-09 19:04:00 -0600

    fixed horizontal scrolling showing white space on mobile

--- src/index.js
+++ src/index.js
@@ -12,18 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'),
         document.body.style.overscrollBehavior = 'contain';
     }
 );
-// Disable touchmove event for horizontal scrolling
-document.addEventListener('touchmove', function(e) {
-    if (e.touches.length === 1) {
-        var touch = e.touches[0];
-        var x = touch.clientX;
-        var y = touch.clientY;
-        var element = document.elementFromPoint(x, y);
-        if (element.tagName.toLowerCase() !== 'input' && element.tagName.toLowerCase() !== 'textarea') {
-            e.preventDefault();
-        }
-    }
-}, { passive: false });
 
 root.render(
   <React.StrictMode>
